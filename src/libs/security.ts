809b42ea180de548e95b0c4f3ad458b7a96950d3
Gabriel Acuña <gacuna@grupozurita.com.ec>
2025-11-05 10:33:46 -0500

    refactor: improve encoding handling in security functions

--- src/libs/security.ts
+++ src/libs/security.ts
@@ -1,30 +1,37 @@
-import * as crypto from 'node:crypto'
-export function sha1ToBase64(text: string): string {
-    const HASH = crypto.createHash('sha1').update(text ).digest('hex')
-    const BUFFER = Buffer.from(HASH, 'hex')
-    return BUFFER.toString('base64')
+import * as crypto from "node:crypto";
+import { Encoding } from "node:crypto";
+export function sha1ToBase64(text: string, encoding: Encoding): string {
+  const HASH = crypto.createHash("sha1").update(text, encoding).digest("hex");
+  const BUFFER = Buffer.from(HASH, "hex");
+  return BUFFER.toString("base64");
 }
 
-export function hexToBase64(hashHex: string){
-    hashHex.padStart(hashHex.length +
-        (hashHex.length %2), '0');
-    const bytes = hashHex.match(/.{2}/g)?.map(
-        byte => parseInt(byte, 16)
-    );
-   return bytes ? Buffer.from(String.fromCharCode( ...bytes), 'base64')
-   .toString('base64') : '';
+export function hexToBase64(hashHex: string) {
+  if (hashHex.length % 2 !== 0) {
+    hashHex = "0" + hashHex;
+  }
+  if (!isHexString(hashHex)) {
+    throw new Error("Invalid hex string");
+  }
+  const buf = Buffer.from(hashHex, "hex");
+  return buf.toString("base64");
 }
 
-export function bigintToBase64(param: bigint){
-    const hexString = param.toString(16);
-    const hexPairs = hexString.match(/\w{2}/g);
-    const bytes = hexPairs?.map( (pair) => parseInt(pair, 16));
-    const byteString = bytes ? String.fromCharCode(...bytes) : ''
-    const base64 = btoa(byteString);
-    const formatedBase64 = base64.match(/.{1,76}/g)?.join('\n');
-    return formatedBase64;
+export function bigintToBase64(param: bigint) {
+  let hexString = param.toString(16);
+  if (hexString.length % 2 !== 0) {
+    hexString = "0" + hexString;
+  }
+  const buffer = Buffer.from(hexString, "hex");
+  const base64 = buffer.toString("base64");
+  const formatedBase64 = base64.match(/.{1,76}/g)?.join("\n");
+  return formatedBase64;
 }
 
-export function getRandomValues(min: number = 990, max: number = 9999){
-    return Math.floor(Math.random() * (max - min + 1) + min);
+export function getRandomValues(min: number = 990, max: number = 9999) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+export function isHexString(str: string) {
+  const hexRegEx = /^[0-9a-fA-F]+$/;
+  return hexRegEx.test(str);
 }
