9c013ceb25e02f169e816438af3ff68f3903409f
gabriel-acuna <gabriel.acuna.reg.27@gmail.com>
2025-11-17 16:42:29 -0500

    refactor:lib/files: streamline error handling and formatting in FileManager class

--- src/libs/files.ts
+++ src/libs/files.ts
@@ -1,24 +1,23 @@
-import { readFile } from 'fs/promises'
+import { readFile } from "fs/promises";
 
 export class FileManager {
-    private data: Buffer | undefined;
-    async openFile(path: string) {
-        try {
-            this.data = await readFile(path);
-            console.log(this.data);
-            return this;
-        } catch (err) {
-            console.log(err);
-        }
+  private data?: Buffer;
+  async openFile(path: string) {
+    try {
+      this.data = await readFile(path);
+      return this;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async toString(encoding: BufferEncoding) {
-        if (!this.data) throw new Error()
-        return this.data.toString(encoding)
+  async toString(encoding: BufferEncoding) {
+    if (!this.data)
+      throw new Error("File data is not loaded. Please call openFile() first.");
+    return this.data.toString(encoding);
+  }
 
-    }
-
-    getFile() {
-        return this.data;
-    }
+  getFile() {
+    return this.data;
+  }
 }
